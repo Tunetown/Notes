cadcf3d94ecf74c35c2cc7008a9054c2bd4a29c1
Tom Weber <tom-vibrant@gmx.de>
2022-11-16 19:13:15 +0100

    Small bugfix

--- ui/app/src/Notes.js
+++ ui/app/src/Notes.js
@@ -27,7 +27,7 @@ class Notes {
 	}
 	
 	constructor() { 
-		this.appVersion = '0.96.1';      // Note: Also update the Cahce ID in the Service Worker to get the updates through to the clients!
+		this.appVersion = '0.96.2';      // Note: Also update the Cahce ID in the Service Worker to get the updates through to the clients!
 
 		this.optionsMasterContainer = "treeoptions_mastercontainer";
 		this.outOfDateFiles = [];
@@ -1334,7 +1334,7 @@ class Notes {
 		}
 		
 		// Default
-		return 35;
+		return this.isMobile() ? 35 : 0;
 	}
 	
 	/**
