d45c7b1f47d9c5c2d4a725a0ab2b61e889f9cb24
Tom Weber <tom-vibrant@gmx.de>
2022-11-17 19:14:10 +0100

    Small fix

--- ui/app/src/Notes.js
+++ ui/app/src/Notes.js
@@ -27,7 +27,7 @@ class Notes {
 	}
 	
 	constructor() { 
-		this.appVersion = '0.96.8';      // Note: Also update the Cahce ID in the Service Worker to get the updates through to the clients!
+		this.appVersion = '0.96.9';      // Note: Also update the Cahce ID in the Service Worker to get the updates through to the clients!
 
 		this.optionsMasterContainer = "treeoptions_mastercontainer";
 		this.outOfDateFiles = [];
